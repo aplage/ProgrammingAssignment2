4e38b33d3c3cd73f2d1fbbda93c10c529c4cd83d
aplage <aplage@users.noreply.github.com>
2015-11-22 15:14:43 -0200

    Update cachematrix.R
    Functions to functions that cache the inverse of a matrix for the R Programming course

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,32 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+     mt<-NULL
+     set<-function(a){
+          x<<-a
+          mt<<-NULL
+     }
+     get<-function() x
+     setmatrix<-function(solve) mt<<- solve
+     getmatrix<-function() mt
+     list(set=set, get=get,
+          setmatrix=setmatrix,
+          getmatrix=getmatrix)
 }
+
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+     ## Return a matrix that is the inverse of 'x'
+     mt<-x$getmatrix()
+     if(!is.null(mt)){
+          message("getting cached data")
+          return(mt)
+     }
+     matrix<-x$get()
+     mt<-solve(matrix, ...)
+     x$setmatrix(mt)
+     mt
 }
